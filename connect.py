ec8195de8f16cb355a647d1965f15032671f1768
Luis Montejano <montejanol@co.monterey.ca.us>
2020-07-31 16:24:41 -0700

    Ass connect.py and show_version_nxos2.txt

--- connect.py
+++ connect.py
@@ -1,24 +1,47 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
+password = getpass()
+
+
+
+
 nxos1 = {
 
 	"host": 'nxos1.lasthop.io',
 	"username": 'pyclass',
-	"password": getpass(),
+	"password": password,
 	"device_type": 'cisco_ios',
 	# session_log='my_session.txt',
 }
 
+nxos2 = {
+
+	"host": 'nxos2.lasthop.io',
+	"username": 'pyclass',
+	"password": password,
+	"device_type": 'cisco_ios',
+}
 
 
+counter = 0
 
-net_connect = ConnectHandler(**nxos1)
-intBrief = net_connect.send_command("show ip int brief")
+for device in [nxos1, nxos2]:
+	
+
+	if (counter <1):
+		net_connect = ConnectHandler(**device)
+		intBrief = net_connect.send_command("show version")
+		print(net_connect.find_prompt() + "\n")
+		print(intBrief)
+	else:
+		with open("show_version_nxos2.txt", "w") as f:
+			f.write(intBrief)
+		net_connect.disconnect()
+	counter = counter + 1
 
 
-print(net_connect.find_prompt())
+		
 
-print(intBrief)
 
 
