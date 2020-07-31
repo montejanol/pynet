24e511cda69bec4043fd076f3b12226f4086f36e
Luis Montejano <montejanol@co.monterey.ca.us>
2020-07-31 15:57:02 -0700

    Changes made to connect.py

--- connect.py
+++ connect.py
@@ -1,13 +1,24 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
-net_connect = ConnectHandler(
+nxos1 = {
 
-	host='nxos1.lasthop.io',
-	username='pyclass',
-	password=getpass(),
-	device_type='cisco_ios',
-	session_log='my_session.txt',
-)
+	"host": 'nxos1.lasthop.io',
+	"username": 'pyclass',
+	"password": getpass(),
+	"device_type": 'cisco_ios',
+	# session_log='my_session.txt',
+}
+
+
+
+
+net_connect = ConnectHandler(**nxos1)
+intBrief = net_connect.send_command("show ip int brief")
+
 
 print(net_connect.find_prompt())
+
+print(intBrief)
+
+
